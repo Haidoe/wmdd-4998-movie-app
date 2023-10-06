ecc51c0e594690fd8912dfd9e86887a7f5447488
haidoe <haidren.amalia@gmail.com>
2023-10-05 20:48:33 -0700

    feat: create item for the list component

--- src/components/list/ItemList.js
+++ src/components/list/ItemList.js
@@ -1,44 +1,12 @@
-import { Button, Image, Text } from "@rneui/themed";
-import { ActivityIndicator, FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
+import Item from "../listItems/Item";
 
 const ItemList = ({ data }) => {
   return (
     <FlatList
       data={data}
       style={styles.list}
-      renderItem={({ item }) => (
-        <View style={styles.itemContainer}>
-          <View style={styles.itemImgContainer}>
-            <Image
-              source={{
-                uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
-              }}
-              style={styles.itemImg}
-              PlaceholderContent={<ActivityIndicator />}
-            />
-          </View>
-
-          <View style={styles.itemDetailsContainer}>
-            <Text style={{ fontWeight: "bold", width: "80%" }}>
-              {item.title ?? item.name}
-            </Text>
-
-            <Text style={{ fontSize: 12 }}>Popularity: {item.popularity}</Text>
-
-            <Text style={{ fontSize: 12 }}>
-              Release Date: {item.release_date}
-            </Text>
-
-            <Button
-              title="More Details"
-              buttonStyle={{
-                backgroundColor: "#31adcd",
-              }}
-              titleStyle={{ fontSize: 14 }}
-            />
-          </View>
-        </View>
-      )}
+      renderItem={({ item }) => <Item {...item} />}
     />
   );
 };
@@ -46,27 +14,6 @@ const ItemList = ({ data }) => {
 const styles = StyleSheet.create({
   list: {
     marginTop: 16,
-  },
-  itemContainer: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 16,
-    marginBottom: 24,
-  },
-
-  itemImgContainer: {
-    width: 100,
-  },
-
-  itemImg: {
-    aspectRatio: 1,
-    flex: 1,
-  },
-
-  itemDetailsContainer: {
-    display: "flex",
-    flex: 1,
-    justifyContent: "space-between",
   },
 });
 
---
+++ src/components/listItems/Item.js
@@ -0,0 +1,62 @@
+import { Button, Image } from "@rneui/themed";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+
+const Item = (props) => {
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.itemImgContainer}>
+        <Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/w500${props.poster_path}`,
+          }}
+          style={styles.itemImg}
+          PlaceholderContent={<ActivityIndicator />}
+        />
+      </View>
+
+      <View style={styles.itemDetailsContainer}>
+        <Text style={{ fontWeight: "bold", width: "80%" }}>
+          {props.title ?? props.name}
+        </Text>
+
+        <Text style={{ fontSize: 12 }}>Popularity: {props.popularity}</Text>
+
+        <Text style={{ fontSize: 12 }}>Release Date: {props.release_date}</Text>
+
+        <Button
+          title="More Details"
+          buttonStyle={{
+            backgroundColor: "#31adcd",
+          }}
+          titleStyle={{ fontSize: 14 }}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 16,
+    marginBottom: 24,
+  },
+
+  itemImgContainer: {
+    width: 100,
+  },
+
+  itemImg: {
+    aspectRatio: 1,
+    flex: 1,
+  },
+
+  itemDetailsContainer: {
+    display: "flex",
+    flex: 1,
+    justifyContent: "space-between",
+  },
+});
+
+export default Item;
